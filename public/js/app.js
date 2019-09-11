4469adf16b83ff119be5327cf57cf25101cd3f48
BN <bryanng328@gmail.com>
2019-09-11 21:22:27 +0800

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,3 @@
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-
-// fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
-    
-//     response.json().then((data)=>{
-//         if(data.error){
-//             console.log(' Well, there is an error.')
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-        
-//             }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -33,7 +13,7 @@ weatherForm.addEventListener('submit', (e)=>{
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+    fetch('/weather?address='+ location).then((response)=>{
     
     response.json().then((data)=>{
         if(data.error){
