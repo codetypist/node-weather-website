a238844cc9ca3cd6680cee12687e355d514b1371
BN <bryanng328@gmail.com>
2019-09-13 09:42:55 +0800

    Added daily highest temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback)=>{
         } else if (body.error){
             callback('there is an error in search terms', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +' degrees. There is '+body.currently.precipProbability+'% chance of rain.' 
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +' degrees. There is '+body.currently.precipProbability+'% chance of rain. The daily maximum temperature is '+body.daily.data[0].temperatureHigh+' degrees.' 
                 
             )
         }
